f1db20666ae0d15b0d1eeb2018d3c41a877c80d2
lheyi <95419790+lheyi@users.noreply.github.com>
2022-11-16 16:51:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2001', date: '09-31',
+          type: '生日', name: '李四', year: '2001', date: '10-31',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
@@ -44,7 +44,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2022-07-08' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '9999-99-99' },
+        { keyword: 'marry_day', date: '2022-10-31' },
       ],
     },
   ],
