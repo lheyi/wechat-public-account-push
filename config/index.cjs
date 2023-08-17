81b2ae11466a747c71730ef7894b255f1145f6e9
lheyi <95419790+lheyi@users.noreply.github.com>
2023-08-17 15:03:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,6 +49,26 @@ const USER_CONFIG = {
     },
   ],
    SLEEP_TIME: 65000,
+   TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '52fa2d8d4aabdb7036eb2bff4d223fac',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: true,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 3,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 3,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
+  },
 
 
 
