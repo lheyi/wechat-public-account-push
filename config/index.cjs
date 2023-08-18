e1e7b19e9662dd63e0379b91bd5fece24bf3bcd2
lheyi <95419790+lheyi@users.noreply.github.com>
2023-08-18 10:43:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -81,7 +81,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oANw-5lfOWFVbrz3SXPuPcuQAbtM',
+      id: 'oANw-5iGAnC1rwhKejKQNoBisM_w',
     }
   ],
 
